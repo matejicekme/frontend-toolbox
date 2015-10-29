18321e165d9a83ef9df73ab40f84cb78ccaf9881
Jordan Matejicek <jordan@matejicek.me>
2015-10-29 18:16:02 +0100

    corrected scripts & css paths

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ gulp.task('images-deploy', function() {
 //compiling our Javascripts
 gulp.task('scripts', function() {
     //this is where our dev JS scripts are
-    return gulp.src(['app/scripts/src/_includes/**/*.js', 'app/scripts/src/**/*.js'])
+    return gulp.src(['app/scripts/_includes/**/*.js', 'app/scripts/**/*.js'])
                 //prevent pipe breaking caused by errors from gulp plugins
                 .pipe(plumber())
                 //this is the filename of the compressed version of our JS
@@ -73,7 +73,7 @@ gulp.task('scripts', function() {
 //compiling our Javascripts for deployment
 gulp.task('scripts-deploy', function() {
     //this is where our dev JS scripts are
-    return gulp.src(['app/scripts/src/_includes/**/*.js', 'app/scripts/src/**/*.js'])
+    return gulp.src(['app/scripts/_includes/**/*.js', 'app/scripts/**/*.js'])
                 //prevent pipe breaking caused by errors from gulp plugins
                 .pipe(plumber())
                 //this is the filename of the compressed version of our JS
@@ -153,7 +153,7 @@ gulp.task('main-bower-fonts-deploy', function() {
 //compiling our SCSS files
 gulp.task('styles', function() {
     //the initializer / master SCSS file, which will just be a file that imports everything
-    return gulp.src('app/styles/scss/init.scss')
+    return gulp.src('app/styles/init.scss')
                 //prevent pipe breaking caused by errors from gulp plugins
                 .pipe(plumber())
                 //get sourceMaps ready
@@ -185,7 +185,7 @@ gulp.task('styles', function() {
 //compiling our SCSS files for deployment
 gulp.task('styles-deploy', function() {
     //the initializer / master SCSS file, which will just be a file that imports everything
-    return gulp.src('app/styles/scss/init.scss')
+    return gulp.src('app/styles/init.scss')
                 .pipe(plumber())
                 //include SCSS includes folder
                 .pipe(sass({
